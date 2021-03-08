561b3344ddc2d4163502f20b69c5d9cc3dbd72be
Tanoo <tanoo@arcadier.com>
2021-03-08 11:31:37 +0800

    added instructions

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.post("/webhook", (req, res) => {
 
 });
 
+app.get("/", (req, res) => {
+    res.send("Use a POST request with \"https://dummy-3rd-party.herokuapp.com/webhook\", send any JSON object, e.g { \"Payload\": \"Received\" } and watch the logs.")
+})
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("Express server listning on port ");
 });
