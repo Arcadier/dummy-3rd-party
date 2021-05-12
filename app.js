3fd86d25c9ad4815a719e4546d14e49b2cf6e532
Tanoo Joyekurun <tanoo@arcadier.com>
2021-05-12 11:15:53 +0800

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.post("/webhook", (req, res) => {
 });
 
 app.get("/", (req, res) => {
+    
     res.send("Use a POST request with \"https://dummy-3rd-party.herokuapp.com/webhook\", send any JSON object, e.g { \"Payload\": \"Sent\" } and excpect the response: { \"message\": \"Payload Received\" }.")
+    console.log("received");
 })
 
 app.listen(process.env.PORT || 3000, () => {
