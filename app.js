25361f5809e0c82ff6bfc7870a8beb15eb6dbafc
Tanoo <tanoo@arcadier.com>
2021-05-12 13:08:31 +0800

    added image

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.post("/webhook", (req, res) => {
 
 app.get("/", (req, res) => {
     res.send("Use a POST request with \"https://dummy-3rd-party.herokuapp.com/webhook\", send any JSON object, e.g { \"Payload\": \"Received\" } and watch the logs.")
+    res.sendFile(path.join(__dirname+'/me.jpg'));
 })
 
 app.listen(process.env.PORT || 3000, () => {
